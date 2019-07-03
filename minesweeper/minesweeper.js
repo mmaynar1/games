9fd6ed3af621a2f8e750e78d2bf7994aa568b52c
MSI\mitchm <mmaynar1@gmail.com>
2019-07-02 22:30:57 -0500

    Assign mines and calculate neighbor mine counts

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -10,7 +10,7 @@ function Cell( row, column, opened, flagged, mined, neighborMineCount )
 	}
 }
 
-function Board( boardSize )
+function Board( boardSize, mineCount )
 {
 	var board = {};
 	for( var row = 0; row < boardSize; row++ )
@@ -20,7 +20,8 @@ function Board( boardSize )
 			board[row + "" + column] = Cell( row, column, false, false, false, 0 );
 		}
 	}
-
+	board = randomlyAssignMines( board, mineCount );
+	board = calculateNeighborMineCounts( board, boardSize );
 	return board;
 }
 
@@ -51,6 +52,70 @@ var initializeCells = function( boardSize )
 	})
 }
 
+var randomlyAssignMines = function( board, mineCount )
+{
+	var mineCooridinates = [];
+	for( var i = 0; i < mines; i++ )
+	{
+		var randomRowCoordinate = getRandomInteger( 0, boardSize );
+		var randomColumnCoordinate = getRandomInteger( 0, boardSize );
+		var cell = randomRowCoordinate + "" + randomColumnCoordinate;
+		while( mineCooridinates.includes( cell ) )
+		{
+			randomRowCoordinate = getRandomInteger( 0, boardSize );
+			randomColumnCoordinate = getRandomInteger( 0, boardSize );
+			cell = randomRowCoordinate + "" + randomColumnCoordinate;
+		}
+		mineCooridinates.push( cell );
+		board[cell].mined = true;
+	}
+	return board;
+}
+
+var calculateNeighborMineCounts = function( board, boardSize )
+{
+	var cell;
+	var neighborMineCount = 0;
+	for( var row = 0; row < boardSize; row++ )
+	{
+		for( var column = 0; column < boardSize; column++ )
+		{
+			cell = board[row + "" + column];
+			if( !cell.mined )
+			{
+				neighborMineCount = 0;
+				neighborMineCount += isMined( board, row - 1, column - 1 );
+				neighborMineCount += isMined( board, row - 1, column );
+				neighborMineCount += isMined( board, row - 1, column + 1 );
+				neighborMineCount += isMined( board, row, column - 1 );
+				neighborMineCount += isMined( board, row, column + 1 );
+				neighborMineCount += isMined( board, row + 1, column - 1 );
+				neighborMineCount += isMined( board, row + 1, column );
+				neighborMineCount += isMined( board, row + 1, column + 1 );
+				cell.neighborMineCount = neighborMineCount;
+			}
+		}
+	}
+	return board;
+}
+
+var isMined = function( board, row, column )
+{	
+	var cell = board[row + "" + column];
+	var mined = 0;
+	if( typeof cell !== 'undefined' )
+	{
+		mined = cell.mined ? 1 : 0;
+	}
+	return mined;
+}
+
+var getRandomInteger = function( min, max )
+{
+	return Math.floor( Math.random() * ( max - min ) ) + min;
+}
+
 var boardSize = 10;
+var mines = 10;
 initializeCells( boardSize );
-var board = Board( boardSize );
+var board = Board( boardSize, mines );
